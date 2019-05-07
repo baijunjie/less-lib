85984f2cf0b240dee47b4074b4a367cc14dffd49
baijunjie <625603381@qq.com>
2019-05-07 19:31:44 +0900

    v1.3.1

--- gulpfile.js
+++ gulpfile.js
@@ -9,22 +9,22 @@ var srcPath = ['./less/*.less', '!./less/_*.less'];
     watchPath = './less/**/*.less';
 
 gulp.task('lessClean', function () {
-	return gulp.src(distPath, { read: false })
+	return gulp.src(distPath, { read: false, allowEmpty: true })
 		.pipe(clean({ force: true }));
 });
 
-gulp.task('less', ['lessClean'], function () {
-	return gulp.src(srcPath)
+gulp.task('less', gulp.series('lessClean', function() {
+    return gulp.src(srcPath)
         // 当编译时出现语法错误或者其他异常，会终止watch事件，通常需要查看命令提示符窗口才能知道，这并不是我们所希望的，
         // 所以我们需要处理出现异常并不终止watch事件（gulp-plumber），并提示我们出现了错误（gulp-notify）。
         .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-		.pipe(less())
-		.pipe(gulp.dest(distPath));
-});
+        .pipe(less({ javascriptEnabled: true }))
+        .pipe(gulp.dest(distPath));
+}));
 
 // 监听文件修改
-gulp.task('lessWatch', ['less'], function () {
-	gulp.watch(watchPath, ['less']);
-});
+gulp.task('lessWatch', gulp.series('less', function() {
+	gulp.watch(watchPath, gulp.parallel('less'));
+}));
 
-gulp.task('default', ['lessWatch']);
+gulp.task('default', gulp.parallel('lessWatch'));
